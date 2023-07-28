916f8f2a505e21a6ce240624b85c83723b3f6ddf
Khishigt Khurelbat <h.uzuu20@gmail.com>
2023-07-28 06:56:30 +0000

    1

---
+++ src/BMI calcutor/Bmi.js
@@ -0,0 +1,11 @@
+import './Bmi.css'
+import Calbmi from './Calbmi';
+
+function Bmi() {
+    return
+    <div>
+        <Calbmi/>
+    </div>
+}
+ 
+export default Bmi;
---
+++ src/BMI calcutor/Calbmi.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react' 
+const Calbmi = () => {
+    return (  
+        <div><h1>BMI Calculator</h1></div>
+    );
+}
+ 
+export default Calbmi;
